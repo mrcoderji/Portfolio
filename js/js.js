dac6ecff5f49571258ce4a1e88a155897fd7d6d5
Arpit Sahu <141658016+mrcoderji@users.noreply.github.com>
2023-10-21 23:28:09 +0530

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 300) {
+//             $(".nav-item").css("background", "blue");
+//         }
+
+//         else {
+//             $(".nav-item").css("background", "#fff");
+//         }
+//     })
+// })
